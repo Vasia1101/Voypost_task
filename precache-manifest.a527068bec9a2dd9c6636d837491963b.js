521ca657a2fba6bbba84fae9545e4e96a3033532
Vasia1101 <vasilij.gajda@gmail.com>
2019-09-05 21:56:28 +0300

    Updates

---
+++ precache-manifest.a527068bec9a2dd9c6636d837491963b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eebf112ddefee6298a609dbd081cbb2",
+    "url": "/Voypost_task/index.html"
+  },
+  {
+    "revision": "4becd383f1bef5b52171",
+    "url": "/Voypost_task/static/css/main.fb4b4ffd.chunk.css"
+  },
+  {
+    "revision": "59bb21292b162ab254a7",
+    "url": "/Voypost_task/static/js/2.cf6c8d49.chunk.js"
+  },
+  {
+    "revision": "4becd383f1bef5b52171",
+    "url": "/Voypost_task/static/js/main.d4336582.chunk.js"
+  },
+  {
+    "revision": "e14c383fd4bea5aebfb4",
+    "url": "/Voypost_task/static/js/runtime~main.3eedd10d.js"
+  }
+]);
